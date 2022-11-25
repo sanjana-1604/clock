06c1ea6292b3fe984e7428f5ec37a8041df2e421
Sanjana Sethiya <porwal.sanjana@yahoo.com>
2022-11-25 14:31:27 +0000

    analog clock

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
-setInterval(setClock,1000)
+setInterval(setClock, 1000);
 
-function setClock()
-{
-    const currentDate = new Date();
-    const secondsRatio = currentDate.getSeconds()/60
-    const minuteRatio = (secondsRatio + currentDate.getMinutes())/60
-    const hourRatio = (secondsRatio+currentDate.getHours())/12
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+
+}
+setClock();
